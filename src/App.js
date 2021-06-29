4102edc2e27fe35f4054cb6b7c8949c15991833b
Joe Churchman <joechurchman@Joes-MBP.hsd1.wa.comcast.net>
2021-06-29 06:40:23 -0700

    appHeader built, App.js refactored

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import AppHeader from './components/AppHeader'
+
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  top: 13em;
+  left: 0;
+  right: 0;
+  padding: 2em;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <AppHeader />
+      <div>
+        This is the AppHeader component
+      </div>
+    </ AppContainer>
   );
 }
 
---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+`
+
+const HeaderBar = styled.header`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 4em;
+  background-color: teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const HeadingText = styled.h1`
+  color: white;
+  margin: 0;
+`
+
+const StrikeText = styled.span`
+  text-decoration: line-through;
+  margin: 0 0.35em;
+`
+
+const HashbrownTag = styled.img`
+  width: auto;
+  height: 8em;
+  border-radius: 3em;
+  margin-top: 5em;
+`
+
+const AppHeader = () => {
+  return (
+    <Header>
+      <HeaderBar>
+        <HeadingText>
+          Get your
+          <StrikeText>
+            Hashbrowns
+          </StrikeText>
+          Hashtags here!
+        </HeadingText>
+      </HeaderBar>
+      <HashbrownTag src={"https://media.giphy.com/media/10GUbOX16lS15C/giphy.gif"} alt="hashbrown" />
+    </ Header>
+  )
+}
+
+export default AppHeader
